09701c4662cd367ed2a9ce27c15c5bbdf18a9d15
Sourcery AI <>
2023-07-28 16:47:18 +0000

    'Refactored by Sourcery'

--- api/index.py
+++ api/index.py
@@ -24,7 +24,7 @@ def callback():
     signature = request.headers['X-Line-Signature']
     # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    app.logger.info(f"Request body: {body}")
     # handle webhook body
     try:
         line_handler.handle(body, signature)
